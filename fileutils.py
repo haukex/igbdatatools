b3e8714a0e41f65a3979f714e1d9f90b24389a9d
Hauke D <haukex@zero-g.net>
2022-12-23 18:04:54 +0100

    Added fileutils

---
+++ fileutils.py
@@ -0,0 +1,82 @@
+#!python3
+"""Library with some file-related utility functions.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2022 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+import os
+import stat
+import sys
+from pathlib import Path
+from collections.abc import Generator, Iterable
+
+AnyPaths = str|bytes|os.PathLike|Iterable[str|bytes|os.PathLike]
+# noinspection PyPep8Naming
+def to_Paths(paths :AnyPaths) -> Generator[Path]:
+    """Convert various inputs to ``pathlib.Path`` objects."""
+    def topath(item):
+        if isinstance(item, str|bytes): return Path(os.fsdecode(item))
+        elif isinstance(item, os.PathLike): return Path(item)
+        else: raise TypeError(f"I don't know how to covert this to a Path: {item!r}")
+    if isinstance(paths, str|bytes|os.PathLike): yield topath(paths)
+    else: yield from map(topath, iter(paths))
+
+def autoglob(files :Iterable[str], *, force=False):
+    """In Windows, automatically apply ``glob`` and ``expanduser``, otherwise do nothing."""
+    from glob import glob
+    from os.path import expanduser
+    if sys.platform.startswith('win32') or force:
+        for f in files:
+            f = expanduser(f)
+            g = glob(f)  # note glob always returns a list
+            # If a *NIX glob doesn't match anything, it isn't expanded,
+            # while glob() returns an empty list, so we emulate *NIX.
+            if g: yield from g
+            else: yield f
+    else:
+        yield from files
+
+class Pushd:
+    """A context manager that temporarily changes the current working directory."""
+    def __init__(self, newdir :str|os.PathLike):
+        self.newdir = newdir
+    def __enter__(self):
+        self.prevdir = os.getcwd()
+        os.chdir(self.newdir)
+        return
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.prevdir)
+        return False  # raise exception if any
+
+def filetypestr(st :os.stat_result) -> str:
+    """Return a string naming the file type reported by ``stat``."""
+    if stat.S_ISDIR(st.st_mode): return "directory"
+    elif stat.S_ISCHR(st.st_mode): return "character special device file"  # pragma: no cover
+    elif stat.S_ISBLK(st.st_mode): return "block special device file"  # pragma: no cover
+    elif stat.S_ISREG(st.st_mode): return "regular file"
+    elif stat.S_ISFIFO(st.st_mode): return "FIFO (named pipe)"
+    elif stat.S_ISLNK(st.st_mode): return "symbolic link"
+    elif stat.S_ISSOCK(st.st_mode): return "socket"  # pragma: no cover
+    # Solaris
+    elif stat.S_ISDOOR(st.st_mode): return "door"  # pragma: no cover
+    # Solaris?
+    elif stat.S_ISPORT(st.st_mode): return "event port"  # pragma: no cover
+    # union/overlay file systems
+    elif stat.S_ISWHT(st.st_mode): return "whiteout"  # pragma: no cover
+    else: raise ValueError(f"unknown filetype {st.st_mode:#o}")  # pragma: no cover
---
+++ tests/test_fileutils.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""Tests for fileutils library.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2022 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+import unittest
+import sys
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from fileutils import to_Paths, autoglob, Pushd, filetypestr
+
+class TestFileUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_to_paths(self):
+        s = __file__
+        b = os.fsencode(__file__)
+        p = Path(__file__)
+        self.assertEqual( (p,), tuple(to_Paths(s)) )
+        self.assertEqual( (p,), tuple(to_Paths(b)) )
+        self.assertEqual( (p,), tuple(to_Paths(p)) )
+        self.assertEqual( (p,p,p), tuple(to_Paths((s,b,p))) )
+        with self.assertRaises(TypeError):
+            tuple( to_Paths((123,)) )
+
+    def test_autoglob(self):
+        testpath = Path(__file__).parent
+        testglob = str(testpath/'*utils*.py')
+        noglob = str(testpath/'zdoesntexist*')
+        files = sorted( str(p) for p in testpath.iterdir() if 'utils' in p.name and p.name.endswith('.py') )
+        self.assertTrue(len(files)>3)
+        # this doesn't really test expanduser but that's ok
+        self.assertEqual( files+[noglob], sorted(autoglob([testglob, noglob], force=True)) )
+        self.assertEqual( files if sys.platform.startswith('win32') else [testglob], list(autoglob([testglob])) )
+
+    def test_pushd(self):
+        prevwd = os.getcwd()
+        with (TemporaryDirectory() as td1, TemporaryDirectory() as td2):
+            # basic pushd
+            with Pushd(td1):
+                self.assertEqual(os.getcwd(), td1)
+                with Pushd(td2):
+                    self.assertEqual(os.getcwd(), td2)
+                self.assertEqual(os.getcwd(), td1)
+            self.assertEqual(os.getcwd(), prevwd)
+            # exception inside the `with`
+            class BogusError(RuntimeError): pass
+            with self.assertRaises(BogusError):
+                with Pushd(td2):
+                    self.assertEqual(os.getcwd(), td2)
+                    raise BogusError()
+            self.assertEqual(os.getcwd(), prevwd)
+            # attempting to change into a nonexistent directory
+            with self.assertRaises(FileNotFoundError):
+                with Pushd('thisdirectorydoesnotexist'):  # the exception happens here
+                    self.fail()  # pragma: no cover
+            # attempting to change back to a directory that no longer exists
+            with TemporaryDirectory() as td3:
+                with self.assertRaises(FileNotFoundError):
+                    with Pushd(td3):
+                        with Pushd(td2):
+                            os.rmdir(td3)
+                    # the exception happens here
+                    self.fail()  # pragma: no cover
+
+    def test_filetypestr(self):
+        with TemporaryDirectory() as td:
+            tp = Path(td)
+            with open(tp/'foo', 'w') as fh: print("foo", file=fh)
+            (tp/'bar').mkdir()
+            self.assertEqual( 'regular file', filetypestr( os.lstat(tp/'foo') ) )
+            self.assertEqual( 'directory', filetypestr( os.lstat(tp/'bar') ) )
+            try:
+                (tp/'baz').symlink_to('foo')
+            except OSError as ex:  # pragma: no cover
+                print(f"Skipping symlink test ({ex})", file=sys.stderr)
+            else:
+                self.assertEqual( 'symbolic link', filetypestr( os.lstat(tp/'baz') ) )
+            if hasattr(os, 'mkfifo'):
+                os.mkfifo(tp/'quz')
+                self.assertEqual( 'FIFO (named pipe)', filetypestr( os.lstat(tp/'quz') ) )
+            else:  # pragma: no cover
+                print("Skipping fifo test (no mkfifo)", file=sys.stderr)
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
