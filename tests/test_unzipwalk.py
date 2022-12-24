e1f7accb99fbf7a9d1b1e63d265cb1b68dce96b4
Hauke D <haukex@zero-g.net>
2022-12-24 20:42:02 +0100

    Added unzipwalk

---
+++ tests/test_unzipwalk.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+"""Tests for unzipwalk.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2022 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+import unittest
+import os
+import sys
+from copy import deepcopy
+from pathlib import Path, PurePosixPath
+from unzipwalk import unzipwalk, FileType
+
+expect = (
+    ( (Path("test.csv"),), b'"ID","Name","Age"\n1,"Foo",23\n2,"Bar",45\n3,"Quz",67\n', FileType.FILE ),
+    ( (Path("WinTest.ZIP"), PurePosixPath("Foo.txt")),
+        b"Foo\r\nBar\r\n", FileType.FILE ),
+    # Note the WinTest.ZIP doesn't contain an entry for the "World/" dir
+    # (this zip was created with Windows Explorer, everything else on Linux)
+    ( (Path("WinTest.ZIP"), PurePosixPath("World/Hello.txt")),
+        b"Hello\r\nWorld", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/")), None, FileType.DIR ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/abc.zip"), PurePosixPath("abc.txt")),
+        b"One two three\nfour five six\nseven eight nine\n", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/abc.zip"), PurePosixPath("def.txt")),
+        b"3.14159\n", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/iii.dat")),
+        b"jjj\nkkk\nlll\n", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/world.txt.gz"), PurePosixPath("archive/world.txt")),
+        b"This is a file\n", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/xyz.txt")),
+        b"XYZ!\n", FileType.FILE ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/fifo")), None, FileType.OTHER ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/test2/")), None, FileType.DIR ),
+    ( (Path("archive.tar.gz"), PurePosixPath("archive/test2/jjj.dat")), None, FileType.SYMLINK ),
+    ( (Path("linktest.zip"), PurePosixPath("linktest/") ), None, FileType.DIR ),
+    ( (Path("linktest.zip"), PurePosixPath("linktest/hello.txt")),
+        b"Hi there\n", FileType.FILE ),
+    ( (Path("linktest.zip"), PurePosixPath("linktest/world.txt")),
+        b"hello.txt", FileType.FILE ),  # this is what zipinfo returns for symlinks
+    ( (Path("more.zip"), PurePosixPath("more/")), None, FileType.DIR ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/")), None, FileType.DIR ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/five.txt")),
+        b"5\n5\n5\n5\n5\n", FileType.FILE ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/six.txt")),
+        b"6\n6\n6\n6\n6\n6\n", FileType.FILE ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/four.txt")),
+        b"4\n4\n4\n4\n", FileType.FILE ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/texts.tgz"), PurePosixPath("one.txt")),
+        b"111\n11\n1\n", FileType.FILE ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/texts.tgz"), PurePosixPath("two.txt")),
+        b"2222\n222\n22\n2\n", FileType.FILE ),
+    ( (Path("more.zip"), PurePosixPath("more/stuff/texts.tgz"), PurePosixPath("three.txt")),
+        b"33333\n3333\n333\n33\n3\n", FileType.FILE ),
+    ( (Path("subdir"),), None, FileType.DIR ),
+    ( (Path("subdir","ooo.txt"),),
+        b"oOoOoOo\n\n", FileType.FILE ),
+    ( (Path("subdir","foo.zip"), PurePosixPath("hello.txt")),
+        b"Hallo\nWelt\n", FileType.FILE ),
+    ( (Path("subdir","foo.zip"), PurePosixPath("foo/")), None, FileType.DIR ),
+    ( (Path("subdir","foo.zip"), PurePosixPath("foo/bar.txt")),
+        b"Blah\nblah\n", FileType.FILE ),
+)
+
+class TestUnzipWalk(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        self.testdir = Path(__file__).parent.resolve()/'zips'
+        self.expect_all = list( deepcopy( expect ) )
+        os.chdir( self.testdir )
+        try:
+            (self.testdir/'baz.zip').symlink_to('more.zip')
+            self.expect_all.append( ( (Path("baz.zip"),), None, FileType.SYMLINK ) )
+        except OSError as ex:  # pragma: no cover
+            print(f"Skipping symlink test ({ex})", file=sys.stderr)
+        if hasattr(os, 'mkfifo'):
+            os.mkfifo(self.testdir/'xy.fifo')
+            self.expect_all.append( ( (Path("xy.fifo"),), None, FileType.OTHER ) )
+        else:  # pragma: no cover
+            print("Skipping fifo test (no mkfifo)", file=sys.stderr)
+        self.expect_all.sort()
+
+    def tearDown(self):
+        (self.testdir/'baz.zip').unlink(missing_ok=True)
+        (self.testdir/'xy.fifo').unlink(missing_ok=True)
+
+    def test_unzipwalk(self):
+        self.assertEqual(
+            sorted( x[0:-1] for x in self.expect_all if x[-1]==FileType.FILE ),
+            sorted( (fns, fh.read()) for fns, fh in unzipwalk(os.curdir) ) )
+
+    def test_unzipwalk_all(self):
+        self.assertEqual(self.expect_all,
+            sorted(
+                ( fns, fh.read() if ft==FileType.FILE else None, ft)
+                for fns, fh, ft in unzipwalk(os.curdir, onlyfiles=False )
+            ) )
+
+    def test_unzipwalk_errs(self):
+        with self.assertRaises(FileNotFoundError):
+            list(unzipwalk('/thisfileshouldnotexist'))
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
---
+++ unzipwalk.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+"""Library to recrsively read compressed files.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2022 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+from enum import Enum
+from gzip import GzipFile
+from itertools import chain
+from pathlib import PurePosixPath, PurePath, Path
+from tarfile import TarFile
+from zipfile import ZipFile
+import typing
+from collections.abc import Generator, Sequence
+from fileutils import AnyPaths, to_Paths
+
+class FileType(Enum):
+    FILE = 0
+    DIR = 1
+    SYMLINK = 2
+    OTHER = 3
+
+def _procfile(fns :Sequence[PurePath], fh :typing.IO[bytes]|GzipFile, *, onlyfiles :bool) \
+        -> Generator[ tuple[ tuple[PurePath, ...], typing.IO[bytes] ]
+                    | tuple[ tuple[PurePath, ...], typing.IO[bytes], FileType ] ]:
+    bfnl = fns[-1].name.lower()
+    if bfnl.endswith('.tar.gz') or bfnl.endswith('.tgz') or bfnl.endswith('.tar'):
+        with TarFile.open(fileobj=fh) as tf:
+            for ti in tf.getmembers():
+                newname = (*fns, PurePosixPath(ti.name))
+                if ti.issym():
+                    if not onlyfiles: yield newname, None, FileType.SYMLINK
+                elif ti.isdir():
+                    if not onlyfiles: yield newname, None, FileType.DIR
+                elif ti.isfile():
+                    with tf.extractfile(ti) as fh2:
+                        yield from _procfile(newname, fh2, onlyfiles=onlyfiles)
+                else:
+                    # for ti.type see e.g.: https://github.com/python/cpython/blob/v3.11.1/Lib/tarfile.py#L87
+                    if not onlyfiles: yield newname, None, FileType.OTHER
+    elif bfnl.endswith('.zip'):
+        with ZipFile(fh) as zf:
+            for zi in zf.infolist():
+                # Note the ZIP specification requires forward slashes for pathnames.
+                # https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+                newname = (*fns, PurePosixPath(zi.filename))
+                if zi.is_dir():
+                    if not onlyfiles: yield newname, None, FileType.DIR
+                else:  # (note this interface doesn't have an is_file)
+                    with zf.open(zi) as fh2:
+                        yield from _procfile(newname, fh2, onlyfiles=onlyfiles)
+    elif bfnl.endswith('.gz'):
+        with GzipFile(fileobj=fh, mode='rb') as fh2:
+            yield from _procfile((*fns, fns[-1].with_suffix('')), fh2, onlyfiles=onlyfiles)
+    else:
+        if onlyfiles: yield fns, fh
+        else: yield fns, fh, FileType.FILE
+
+def unzipwalk(paths :AnyPaths, *, onlyfiles :bool=True) \
+        -> Generator[ tuple[ tuple[PurePath, ...], typing.IO[bytes] ]
+                    | tuple[ tuple[PurePath, ...], typing.IO[bytes], FileType ] ]:
+    """Recursively step into directories and compressed files and yield names and filehandles.
+
+    When ``onlyfiles`` is ``True`` (the default), this generator will yield tuples consisting of two elements:
+    1. a tuple of the filename(s) (described below), and 2. the filehandle for reading the file contents,
+    open in binary mode. Whether this filehandle supports operations like seeking depends on the underlying library.
+
+    When ``onlyfiles`` is ``False``, the above tuples will contain a third element: a ``FileType`` value.
+    When this value is *not* ``FileType.FILE``, then the second element, which is normally the filehandle,
+    will be ``None``.
+
+    The first element of the yielded tuple is always a tuple of ``pathlib`` objects. If this tuple has only
+    one element, then the yielded file physically exists in the filesystem, and if the tuple has more than
+    one element, then the yielded file is contained in a compressed file, possibly nested in other compressed file(s),
+    and the last element of the tuple will contain the file's actual name.
+
+    The yielded filehandles can for example be wrappend in ``io.TextIOWrapper`` to read them as text files,
+    or even CSV files, for example:
+
+        >>> from io import TextIOWrapper
+        >>> import csv
+        >>> for fnames, binfhnd in unzipwalk('.'):
+        ...     if fnames[-1].suffix.lower() == '.csv':
+        ...         with TextIOWrapper(binfhnd, 'UTF-8', newline='') as fhnd:
+        ...             print(repr(fnames))
+        ...             csvrd = csv.reader(fhnd, strict=True)
+        ...             for row in csvrd: print(repr(row))           # doctest:+ELLIPSIS
+        (...)
+        [...]
+
+    Currently supported are ZIP, tar, tar+gz, and gz compressed files. Symlinks in ZIP files are (currently) not
+    supported by ``zipfile`` and will be reported as regular files with the file contents being the link target.
+    """
+    paths = tuple(to_Paths(paths))
+    for p in paths: p.resolve(strict=True)  # force FileNotFound errors early
+    for p in chain.from_iterable( pa.rglob('*') if pa.is_dir() else (pa,) for pa in paths ):
+        if p.is_symlink():
+            if not onlyfiles: yield (p,), None, FileType.SYMLINK
+        elif p.is_dir():
+            if not onlyfiles: yield (p,), None, FileType.DIR
+        elif p.is_file():
+            with p.open('rb') as fh:
+                yield from _procfile((p,), fh, onlyfiles=onlyfiles)
+        else:
+            if not onlyfiles: yield (p,), None, FileType.OTHER
+
+if __name__ == '__main__':  # pragma: no cover
+    import sys
+    import argparse
+    import errorutils
+    errorutils.init_handlers()
+    parser = argparse.ArgumentParser(description='unzipwalk')
+    parser.add_argument('-a','--allfiles',help="also list dirs, symlinks, etc.",action="store_true")
+    parser.add_argument('-d','--dump',help="also dump file contents",action="store_true")
+    parser.add_argument('paths', metavar='PATH', help='paths', nargs='*')
+    args = parser.parse_args()
+    thepaths = args.paths if args.paths else Path()
+    if args.allfiles:
+        for filenames, handle, ftype in unzipwalk(thepaths, onlyfiles=False):
+            if args.dump and ftype==FileType.FILE:
+                print(f"{filenames!r} {ftype} {handle.read()!r}")
+            else: print(f"{filenames!r} {ftype}")
+    else:
+        for filenames, handle in unzipwalk(thepaths):
+            if args.dump: print(f"{filenames!r} {handle.read()!r}")
+            else: print(f"{filenames!r}")
+    sys.exit(0)
