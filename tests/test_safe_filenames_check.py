24e7de639188a9b7c23a1e1cae2ed4b7716c1ebb
Hauke D <haukex@zero-g.net>
2022-12-27 13:25:40 +0100

    Fix a test in Windows

--- tests/test_safe_filenames_check.py
+++ tests/test_safe_filenames_check.py
@@ -35,7 +35,7 @@ expect :tuple[ tuple[ tuple[PurePath, ...], str ], ... ] = (
     ((Path('examples.tgz'), PurePosixPath('Hello.txt ')), "filename not allowed in Windows: 'Hello.txt '"),
     ((Path('examples.tgz'), PurePosixPath('Hello.txt.')), "filename not allowed in Windows: 'Hello.txt.'"),
     ((Path('examples.tgz'), PurePosixPath('Hello\nWorld.txt')), "filename not allowed in Windows: 'Hello\\nWorld.txt'"),
-    ((Path('examples.tgz'), PurePosixPath('Hello.TXT')), "case collision in (PosixPath('examples.tgz'),): ['HELLO.TXT', 'hello.txt']"),
+    ((Path('examples.tgz'), PurePosixPath('Hello.TXT')), f"case collision in ({Path('examples.tgz')!r},): ['HELLO.TXT', 'hello.txt']"),
     ((Path('examples.tgz'), PurePosixPath('Héllö.txt')), "non-ASCII characters ('é',"" 'ö'"")"),
     ((Path('examples.tgz'), PurePosixPath('bar.txt')), 'FileType.SYMLINK'),
     ((Path('examples.tgz'), PurePosixPath('foo.fifo')), 'FileType.OTHER'),
