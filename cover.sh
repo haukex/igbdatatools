c0dc5a95da12cbc587d722d8747c98ced6e725e0
Hauke D <haukex@zero-g.net>
2023-01-12 19:50:59 +0000

    Added cover.sh

---
+++ cover.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+coverage run --branch -m unittest "$@"
+if coverage report --skip-covered --show-missing --fail-under=100
+then
+	coverage erase
+else
+	coverage html
+fi
--- prove.sh
+++ prove.sh
@@ -2,4 +2,3 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 # to run individual tests: ./prove.sh tests.test_foo tests.test_bar
 python3 -m unittest -v "$@"
-# coverage run --branch -m unittest && coverage html
