haukex/igbdatatools

commit 9af9f59874d579ba355747f68dc162ec25385a13

Updated HashedFile.validate API

hashedfile.py
@@ -87,18 +87,23 @@ class HashedFile(NamedTuple):
         """Return a line representing this object."""
         return self.hsh.hex() + " " + ("*" if self.binflag else " ") + str(self.fn)
 
-    def validate(self, *, force :bool=False, fail_soft :bool=False) -> Self:
+    def validate(self, *, force :bool=False, fail_soft :bool=False) -> Self|tuple[Self, bytes]:
         """Validate whether this object's hash matches the hash of the file in the filesystem.
 
         Does **not** modify this object but returns a new one!
+
+        If you set ``fail_soft``, then on success, this function will return a two-item tuple consisting of
+        the original object with ``valid`` set to ``False``, and the hash that was calculated from the file.
 
         The ``binflag`` is ignored and not modified."""
         if self.valid and not force: return self
         gothsh = self.hash_file(self.fn, algo=self.algo)
         if gothsh != self.hsh:
-            if fail_soft: return self._replace(valid=False)
+            if fail_soft: return self._replace(valid=False), gothsh
             else: raise ValueError(f"failed to validate {self.fn}: expected {self.hsh.hex()}, got {gothsh.hex()}")
-        else: return self._replace(valid=True)
+        else:
+            if fail_soft: return self._replace(valid=True), gothsh
+            else: return self._replace(valid=True)
 
     @property
     def algo(self):

tests/test_hashedfile.py
@@ -89,6 +89,12 @@ class TestHashedFile(unittest.TestCase):
         self.assertEqual( hf2, hf2v )
         self.assertTrue( hf2v.valid )
         self.assertTrue( hf2v.binflag )
+        hf2vs, actualhsh = hf2.validate(fail_soft=True)
+        self.assertIsNot( hf2, hf2vs )
+        self.assertEqual( hf2, hf2vs )
+        self.assertEqual( hf2.hsh, actualhsh )
+        self.assertTrue( hf2vs.valid )
+        self.assertTrue( hf2vs.binflag )
         bag = { hf1, hf2 }
         self.assertEqual( len(bag), 1 )
         self.assertEqual( bag.pop(), hf2v )
@@ -98,9 +104,10 @@ class TestHashedFile(unittest.TestCase):
         self.assertIsNone( hf3.valid )
         with self.assertRaises(ValueError) as ctx: hf3.validate()
         self.assertEqual( str(ctx.exception), f"failed to validate {file}: expected {badhsh.hex()}, got {hsh.hex()}" )
-        hf3v = hf3.validate(fail_soft=True)
+        hf3v, actualhsh2 = hf3.validate(fail_soft=True)
         self.assertIsNot( hf3v, hf3 )
         self.assertEqual( hf3v, hf3 )
+        self.assertEqual( actualhsh2, hsh )
         self.assertIsNotNone( hf3v.valid )
         self.assertFalse( hf3v.valid )
         self.assertEqual( hf3v.rehash(), hf1 )

commit 2371b360c0965d3f3278d3f939487e2efc7e5c22

Fix inconsistency in hashedfile.validate API
This fixes the bug introduced in 9af9f59874d579ba355747f68dc162ec25385a13

hashedfile.py
@@ -96,9 +96,14 @@ class HashedFile(NamedTuple):
 
         If you set ``fail_soft``, then on success, this function will return a two-item tuple consisting of
         the original object with ``valid`` set to ``False``, and the hash that was calculated from the file.
+        *Warning:* If ``fail_soft`` is true and ``force`` is false, and this object's ``valid`` flag is
+        already set to ``True``, then no hashing on the disk is performed, and the second item of the returned
+        tuple will simply be this hash's precomputed hash value. To always go out to the disk, set ``force``.
 
         The ``binflag`` is ignored and not modified."""
-        if self.valid and not force: return self
+        if self.valid and not force:
+            if fail_soft: return self, self.hsh
+            else: return self
         gothsh = self.hash_file(self.fn, algo=self.algo)
         if gothsh != self.hsh:
             if fail_soft: return self._replace(valid=False), gothsh

tests/test_hashedfile.py
@@ -95,6 +95,9 @@ class TestHashedFile(unittest.TestCase):
         self.assertEqual( hf2.hsh, actualhsh )
         self.assertTrue( hf2vs.valid )
         self.assertTrue( hf2vs.binflag )
+        hf2vs2, actualhsh2 = hf2vs.validate(fail_soft=True)
+        self.assertIs( hf2vs, hf2vs2 )
+        self.assertEqual( hf2vs2.hsh, hf2.hsh )
         bag = { hf1, hf2 }
         self.assertEqual( len(bag), 1 )
         self.assertEqual( bag.pop(), hf2v )