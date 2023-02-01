7f710d2d8e7781fcc80f1b6814079d9ba584a419
Hauke D <haukex@zero-g.net>
2023-02-01 15:13:00 +0100

    Added jsonvalidate

---
+++ jsonvalidate.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""Utility functions to load and validate JSON Schemas.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2023 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+import os
+import io
+import json
+import typing
+from pprint import pprint
+import jschon
+
+def load_json(file :str|os.PathLike|io.IOBase|typing.IO|bytes|bytearray):
+    """Utility function to load JSON either from a filename, file object, or ``bytes`` object."""
+    if isinstance(file, str|os.PathLike):
+        with open(file) as fh: return json.load(fh)
+    elif isinstance(file, io.IOBase|typing.IO):
+        return json.load(file)
+    elif isinstance(file, bytes|bytearray):
+        return json.load(io.BytesIO(file))
+    else: raise TypeError(f"file must be a filename, file object, or bytes, not {repr(file)}")
+
+_catalog = jschon.create_catalog('2020-12')
+def load_json_schema(file, *, verbose=False) -> jschon.JSONSchema:
+    """Loads a JSON Schema and checks that the schema itself is valid, and raises an error if not.
+
+    ``file`` can be either a filename, file object, or ``bytes``.
+    Turning on ``verbose`` outputs status messages to STDOUT."""
+    schema = jschon.JSONSchema(load_json(file), catalog=_catalog)
+    if schema.validate().valid:
+        if verbose: print(f"Schema {file!r} itself is valid")  # pragma: no cover
+    else: raise RuntimeError(f"Schema {file!r} is invalid")
+    return schema
+
+def validate_json(schema :jschon.JSONSchema, file, *, verbose=False):
+    """Validates a JSON file against a schema loaded with :func:`load_json_schema`.
+
+    If the validation fails, returns ``None``, otherwise returns the data structure returned by ``json.load``.
+
+    ``file`` can be either a filename, file object, or ``bytes``.
+    Turning on ``verbose`` outputs status messages to STDOUT."""
+    thejson = load_json(file)
+    result = schema.evaluate(jschon.JSON(thejson))
+    if result.valid:
+        if verbose:  # pragma: no cover
+            print(f"The JSON {file!r} validates against the schema")
+        return thejson
+    else:
+        if verbose:  # pragma: no cover
+            print(f"The JSON {file!r} failed to validate!")
+            pprint(result.output('basic'))  # can also use "detailed" here
+        return None
+
+if __name__ == '__main__':  # pragma: no cover
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description='JSON Validator')
+    parser.add_argument('-q', '--quiet', help="be quiet", action="store_true")
+    parser.add_argument('schema', help="the JSON Schema")
+    parser.add_argument('jsons', help="the JSON files to validate", nargs="+")
+    args = parser.parse_args()
+    schema_ = load_json_schema(args.schema, verbose = not args.quiet)
+    allok = True
+    for j in args.jsons:
+        if validate_json(schema_, j, verbose = not args.quiet) is None:
+            allok = False
+    sys.exit(0 if allok else 1)
---
+++ tests/test_jsonvalidate.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Tests for jsonvalidate.
+
+Author, Copyright, and License
+------------------------------
+Copyright (c) 2023 Hauke Daempfling (haukex@zero-g.net)
+at the Leibniz Institute of Freshwater Ecology and Inland Fisheries (IGB),
+Berlin, Germany, https://www.igb-berlin.de/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/
+"""
+import unittest
+import pkgutil
+import io
+from pathlib import Path
+from jsonvalidate import load_json_schema, validate_json
+
+class TestJsonValidate(unittest.TestCase):
+
+    def test_load_validate_json_schema(self):
+        schema = load_json_schema( pkgutil.get_data('tests', 'test.schema.json') )
+        self.assertEqual( validate_json(schema, io.BytesIO(
+            b'{"foo":{"Hello":"World"},"bar":[1,2,3]}')),
+              {"foo":{"Hello":"World"},"bar":[1,2,3]} )
+        self.assertIsNone( validate_json(schema, b'{"foo":{"Hello":"World"},"bar":[1,2,1]}') )
+        self.assertIsNone( validate_json(schema, b'{"foo":{"0Hello":"World"},"bar":[1,2,3]}') )
+        # also test loading with filename
+        self.assertEqual( schema, load_json_schema( Path(__file__).parent/'test.schema.json' ) )
+        # errors
+        with self.assertRaises(TypeError): load_json_schema( 123 )
+        with self.assertRaises(RuntimeError):
+            load_json_schema( io.BytesIO(b'{"$schema":"https://json-schema.org/draft/2020-12/schema","type":"foobar"}') )
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
