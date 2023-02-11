3766bcb0c68a383c49eab073411156327efb7c8e
Hauke D <haukex@zero-g.net>
2023-02-11 13:24:03 +0100

    Made test verbosity optional

--- prove.sh
+++ prove.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 if [ "$OSTYPE" == "msys" ]  # e.g. Git bash on Windows
 then
-  python3 -m unittest -v "$@"
+  python3 -m unittest "$@"
 else
-  coverage run --branch -m unittest -v "$@"
+  coverage run --branch -m unittest "$@"
   if coverage report --skip-covered --show-missing --fail-under=100
   then
     coverage erase
