0198bcfbdb807640225ee64117e26b28cdaa68a0
Hauke D <haukex@zero-g.net>
2023-02-11 18:54:23 +0100

    Added Record.typecheck and .tzconv

--- loggerdata/importdefs.py
+++ loggerdata/importdefs.py
@@ -22,13 +22,15 @@ along with this program. If not, see https://www.gnu.org/licenses/
 """
 from enum import Enum
 from collections.abc import Sequence
-from typing import Optional
+from typing import Optional, Self
 from more_itertools import first
-from fileutils import Filename
-from loggerdata.metadata import MdTable
-from loggerdata import toa5
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from functools import cached_property
+from datetime import datetime, timezone
+from fileutils import Filename
+from loggerdata import toa5
+from loggerdata.metadata import MdTable
+from datatypes import TimestampNoTz, TimestampWithTz
 
 class DataImportError(RuntimeError): pass
 class NoMetadataMatch(DataImportError): pass
@@ -50,22 +52,59 @@ class Record:
     filenames :Optional[Filename|Sequence[Filename]]
     srcline :int
     filetype :DataFileType
+
     @cached_property
     def fullrow(self) -> tuple[str|None, ...]:
+        """While ``origrow`` represents the columns read from the input file,
+        this property represents the columns as are stored in the table metadata."""
         if len(self.origrow) != len(self.variant):
             raise RecordError("row column count mismatch")
         newrow :list[str|None] = [None] * len(self.tblmd.columns)
         for old_i, new_i in enumerate(self.variant):
             newrow[new_i] = self.origrow[old_i]
         return tuple(newrow)
+
     @cached_property
     def source(self) -> str:
+        """A string representing the source filename(s) and line number for this record."""
         if isinstance(self.filenames, Filename) or not self.filenames:
             return str(self.filenames)+":"+str(self.srcline)
         elif len(self.filenames)==1:
             return str(first(self.filenames))+":"+str(self.srcline)
         else:
             return str(self.filenames)+":"+str(self.srcline)
+
+    def typecheck(self) -> Self:
+        """Run type checks and raise an error if a column does not match its declared type.
+
+        Returns this object to allow method chaining."""
+        for val, vi in zip(self.origrow, self.variant, strict=True):
+            col = self.tblmd.columns[vi]
+            if col.type:  # To-Do for Later: column types should eventually become mandatory (see metadata for similar note)
+                if not col.type.check(val):
+                    raise ValueError(f"value {val!r} does not match {col.type}")
+        return self
+
+    def tzconv(self) -> Self:
+        """Convert timestamp columns into UTC time.
+
+        *Note* that even ``TimestampNoTz`` column values will be turned into UTC with
+        a ``Z`` appended, i.e. they will no longer match the ``TimestampNoTz`` type!
+
+        Returns a *new* object."""
+        neworigrow :list[str] = []
+        for val, vi in zip(self.origrow, self.variant, strict=True):
+            col = self.tblmd.columns[vi]
+            if col.type:  # To-Do for Later: column types should eventually become mandatory (see metadata for similar note)
+                if isinstance(col.type, TimestampNoTz):
+                    assert self.tblmd.parent.tz
+                    val = datetime.fromisoformat(val).replace(tzinfo=self.tblmd.parent.tz) \
+                        .astimezone(timezone.utc).strftime("%Y-%m-%d %H:%M:%SZ")
+                elif isinstance(col.type, TimestampWithTz):
+                    val = datetime.fromisoformat(val) \
+                        .astimezone(timezone.utc).strftime("%Y-%m-%d %H:%M:%SZ")
+            neworigrow.append(val)
+        return replace(self, origrow=tuple(neworigrow))
 
 @dataclass(kw_only=True, frozen=True)
 class Toa5Record(Record):
--- tests/test_logger_importdefs.py
+++ tests/test_logger_importdefs.py
@@ -32,18 +32,44 @@ class TestLoggerDataImportDefs(unittest.TestCase):
 
     def test_record_row(self):
         self.assertEqual( Record(origrow=("abc","def"), tblmd=self.md.tables['foo'], variant=(1,3),
-                                 filenames=(), srcline=1, filetype=DataFileType.CSV).fullrow,
+                                 filenames=(), srcline=1, filetype=DataFileType.TOA5).fullrow,
                           (None,"abc",None,"def",None) )
         with self.assertRaises(RecordError):
             _ = Record(origrow=("abc","def"), tblmd=self.md.tables['foo'], variant=(0,1,3),
-                       filenames=(), srcline=1, filetype=DataFileType.CSV).fullrow
+                       filenames=(), srcline=1, filetype=DataFileType.TOA5).fullrow
 
     def test_record_source(self):
         rec = Record(origrow=(), tblmd=self.md.tables['foo'], variant=(),
-            filenames=("foo.zip","bar.csv"), srcline=42, filetype=DataFileType.CSV)
+            filenames=("foo.zip","bar.csv"), srcline=42, filetype=DataFileType.TOA5)
         self.assertEqual( rec.source, "('foo.zip', 'bar.csv'):42" )
         self.assertEqual( replace(rec, filenames=('bar.csv',)).source, "bar.csv:42" )
         self.assertEqual( replace(rec, filenames='bar.csv').source, "bar.csv:42" )
+
+    def test_record_typecheck_tzconv(self):
+        with self.assertWarns(UserWarning):
+            md2 = load_logger_metadata(b'{"logger_name":"Bar","toa5_env_match":{"station_name":"Bar"},"tz":"-03:30","tables":{"bar":'
+                b'{"prikey":0,"columns":[ {"name":"Hello","type":"TimestampNoTz"}, {"name":"xyz"}, {"name":"World","type":"TimestampWithTz"}, {"name":"iii","type":"NonNegInt"} ]}}}')
+        with self.assertRaises(ValueError):
+            Record(origrow=("2023-01-02 03:04:05","","2023-01-02 03:04:56+04:3","42"), tblmd=md2.tables['bar'],
+                   variant=(0,1,2,3), filenames=(), srcline=3, filetype=DataFileType.TOA5).typecheck()
+        rec = Record(origrow=("2023-01-02 03:04:05","","2023-01-02 03:04:56+04:30","42"), tblmd=md2.tables['bar'],
+                     variant=(0,1,2,3), filenames=(), srcline=3, filetype=DataFileType.TOA5).typecheck().tzconv()
+        self.assertEqual( rec.fullrow, ("2023-01-02 06:34:05Z","","2023-01-01 22:34:56Z","42") )
+        with self.assertRaises(ValueError):
+            # because now the TimestampNoTz column has a "Z" appended!
+            rec.typecheck()
+        # and a few more tzconv checks
+        with self.assertWarns(UserWarning):
+            md3 = load_logger_metadata(b'{"logger_name":"Quz","toa5_env_match":{"station_name":"Quz"},"tz":"Europe/Berlin","tables":{"quz":'
+                b'{"columns":[ {"name":"TIMESTAMP","unit":"TS","type":"TimestampNoTz"}, {"name":"Other","type":"TimestampWithTz"} ]}}}')
+        tzconv_tests = (
+            (("2022-12-01 14:00:00","2021-06-18 14:00:00 -10:00"), ("2022-12-01 13:00:00Z","2021-06-19 00:00:00Z")),
+            (("2022-06-01 13:00:00","2021-06-19 07:00:00+07:00"),  ("2022-06-01 11:00:00Z","2021-06-19 00:00:00Z")),
+            (("2021-06-20 00:00:00","2021-06-19 00:00:00Z"),       ("2021-06-19 22:00:00Z","2021-06-19 00:00:00Z")),
+        )
+        for row, exp in tzconv_tests:
+            self.assertEqual(Record(origrow=row, tblmd=md3.tables['quz'], variant=(0,1), filenames=(),
+                srcline=5, filetype=DataFileType.TOA5).typecheck().tzconv().fullrow, exp )
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
